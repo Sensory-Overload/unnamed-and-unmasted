9232108df6ebc7d8786beae8123859234be613f6
badateverything <62347712+Spookakra@users.noreply.github.com>
2023-09-03 12:54:00 -0400

    i just did a thing

--- temp/CharacterController/Top Down/Top_Down_Camera.gd
+++ temp/CharacterController/Top Down/Top_Down_Camera.gd
@@ -1,13 +1,13 @@
 extends Camera2D
 
 
-const DEAD_ZONE = 50
+const DEAD_NAME = 50
 
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var _target = event.position - get_viewport().size * 0.5
-		if _target.length() < DEAD_ZONE:
+		if _target.length() < DEAD_NAME:
 			self.position = Vector2(0,0)
 		else:
-			self.position = _target.normalized() * (_target.length() -  DEAD_ZONE) * 0.5
+			self.position = _target.normalized() * (_target.length() -  DEAD_NAME) * 0.5
